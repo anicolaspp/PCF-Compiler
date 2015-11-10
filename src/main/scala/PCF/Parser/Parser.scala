465d320a78693a2397a3ce1eae5bb3c5a4ad6deb
Nicolas A Perez <nperez@ipcoop.com>
2015-11-10 11:59:59 -0500

    Adding IF THEN ELSE AST Generator (case 7) and tests

--- src/main/scala/PCF/Parser/Parser.scala
+++ src/main/scala/PCF/Parser/Parser.scala
@@ -12,13 +12,28 @@ import PCF.Pipes.IPipe._
 object Parser {
 
   private def parseExpression(t: List[TOK]): (TERM, List[TOK]) = t match {
-    case IDTOK(x)::toks     =>  (ID(x), toks)
-    case NUMTOK(n)::toks    =>  (NUM(n), toks)
-    case TRUETOK()::toks    =>  (BOOL(true), toks)
-    case FALSETOK()::toks   =>  (BOOL(false), toks)
-    case SUCCTOK()::toks    =>  (SUCC(), toks)
-    case PREDTOK()::toks    =>  (PRED(), toks)
-    case ISZEROTOK()::toks  =>  (ISZERO(), toks)
+    case IDTOK(x)   ::toks    =>  (ID(x), toks)
+    case NUMTOK(n)  ::toks    =>  (NUM(n), toks)
+    case TRUETOK()  ::toks    =>  (BOOL(true), toks)
+    case FALSETOK() ::toks    =>  (BOOL(false), toks)
+    case SUCCTOK()  ::toks    =>  (SUCC(), toks)
+    case PREDTOK()  ::toks    =>  (PRED(), toks)
+    case ISZEROTOK()::toks    =>  (ISZERO(), toks)
+
+    case IFTOK()    ::toks    =>  parseExpression(toks) match {
+      case (ERROR(reason), left)      =>  (ERROR(reason), left)
+      case (guard, THENTOK()::left)   =>  parseExpression(left) match {
+        case (ERROR(reason), dontCare)          =>  (ERROR("IF requires an TERM after THEN"), dontCare)
+        case (thenExp, ELSETOK()::left)         =>  parseExpression(left) match {
+          case (ERROR(reason), left)                =>  (ERROR("IF requires an TERM after ELSE"), left)
+          case (elseExp, left)                      => (IF(guard, thenExp, elseExp), left)
+        }
+        case (thenExpe, _)            =>  (ERROR("IF requires an ELSE"), left)
+      }
+      case (guard, tok)               =>  (ERROR("IF requires an TERM as guard"), List(EOF()))
+    }
+
+    case tok::toks            =>  (ERROR("Expected Expression but found: " + tok), List(EOF()))
   }
 
   private def parse(toks: List[TOK]) = parseExpression(toks) match {
@@ -30,3 +45,4 @@ object Parser {
 
   def parseStr(sourceCode: String) = sourceCode |> Lexer.lexerStr |> parse
 }
+
